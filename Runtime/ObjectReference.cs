a2bd55ac99a0c28622791664f242a53ae86d5ff2
TerentevVlad <ter.vlad.ter@gmail.com>
2021-10-25 14:02:12 +0300

    Init

---
+++ Runtime/ObjectReference.cs
@@ -0,0 +1,143 @@
+using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+#endif
+
+
+namespace LittleBit.Modules.ObjectReferenceMoudle
+{
+    [Serializable]
+    public class ObjectReference : ISerializationCallbackReceiver
+    {
+        // This should only ever be set during serialization/deserialization!
+        [SerializeField] private string objectPath = string.Empty;
+#if UNITY_EDITOR
+        // What we use in editor to select the scene
+        [SerializeField] private Object objectAsset;
+        private bool IsValidSceneAsset
+        {
+            get
+            {
+                if (!objectAsset) return false;
+
+                return objectAsset is Object;
+            }
+        }
+#endif
+
+       
+
+        // Use this when you want to actually have the scene path
+        public string ObjectPath
+        {
+            get
+            {
+#if UNITY_EDITOR
+                // In editor we always use the asset's path
+                return GetObjectPathFromAsset();
+#else
+            // At runtime we rely on the stored path value which we assume was serialized correctly at build time.
+            // See OnBeforeSerialize and OnAfterDeserialize
+            return objectPath;
+#endif
+            }
+            set
+            {
+                objectPath = value;
+#if UNITY_EDITOR
+                objectAsset = GetObjectAssetFromPath();
+#endif
+            }
+        }
+
+        public string PrefabPathInResources
+        {
+            get
+            {
+                string[] pathWithoutResources = ObjectPath.Split(new[] {"Resources/"}, StringSplitOptions.None);
+                if (pathWithoutResources.Length >= 2)
+                {
+                    string path = pathWithoutResources[1];
+                    string[] pathWithoutPrefab = path.Split(new[] {".prefab"}, StringSplitOptions.None);
+                    if (pathWithoutPrefab.Length >= 1)
+                    {
+                        return pathWithoutPrefab[0];
+                    }
+
+                    return string.Empty;
+                }
+
+                return string.Empty;
+            }
+        }
+
+        public static implicit operator string(ObjectReference objectReference)
+        {
+            return objectReference.ObjectPath;
+        }
+
+        // Called to prepare this data for serialization. Stubbed out when not in editor.
+        public void OnBeforeSerialize()
+        {
+#if UNITY_EDITOR
+            HandleBeforeSerialize();
+#endif
+        }
+
+        // Called to set up data for deserialization. Stubbed out when not in editor.
+        public void OnAfterDeserialize()
+        {
+#if UNITY_EDITOR
+            // We sadly cannot touch assetdatabase during serialization, so defer by a bit.
+            EditorApplication.update += HandleAfterDeserialize;
+#endif
+        }
+
+
+
+#if UNITY_EDITOR
+        private Object GetObjectAssetFromPath()
+        {
+            return string.IsNullOrEmpty(objectPath) ? null : AssetDatabase.LoadAssetAtPath<Object>(objectPath);
+        }
+
+        private string GetObjectPathFromAsset()
+        {
+            return objectAsset == null ? string.Empty : AssetDatabase.GetAssetPath(objectAsset);
+        }
+
+        private void HandleBeforeSerialize()
+        {
+            // Asset is invalid but have Path to try and recover from
+            if (IsValidSceneAsset == false && string.IsNullOrEmpty(objectPath) == false)
+            {
+                objectAsset = GetObjectAssetFromPath();
+                if (objectAsset == null) objectPath = string.Empty;
+
+                EditorSceneManager.MarkAllScenesDirty();
+            }
+            // Asset takes precendence and overwrites Path
+            else
+            {
+                objectPath = GetObjectPathFromAsset();
+            }
+        }
+
+        private void HandleAfterDeserialize()
+        {
+            EditorApplication.update -= HandleAfterDeserialize;
+            if (IsValidSceneAsset) return;
+            if (string.IsNullOrEmpty(objectPath)) return;
+
+            objectAsset = GetObjectAssetFromPath();
+
+            if (!objectAsset) objectPath = string.Empty;
+
+            if (!Application.isPlaying) EditorSceneManager.MarkAllScenesDirty();
+        }
+#endif
+    }
+}
---
+++ Runtime/ObjectReferencePropertyDrawer.cs
@@ -0,0 +1,105 @@
+﻿#if UNITY_EDITOR
+
+using UnityEditor;
+using UnityEngine;
+
+namespace LittleBit.Modules.ObjectReferenceMoudle
+{
+
+    /// <summary>
+    /// Display a Scene Reference object in the editor.
+    /// If scene is valid, provides basic buttons to interact with the scene's role in Build Settings.
+    /// </summary>
+    [CustomPropertyDrawer(typeof(ObjectReference))]
+    public class ObjectReferencePropertyDrawer : PropertyDrawer
+    {
+        private const string sceneAssetPropertyString = "objectAsset";
+        private const string scenePathPropertyString = "objectPath";
+
+        private static readonly RectOffset boxPadding = EditorStyles.helpBox.padding;
+
+        private const float PAD_SIZE = 2f;
+        private const float FOOTER_HEIGHT = 10f;
+
+        private static readonly float lineHeight = EditorGUIUtility.singleLineHeight;
+        private static readonly float paddedLine = lineHeight + PAD_SIZE;
+
+        /// <summary>
+        /// Drawing the 'SceneReference' property
+        /// </summary>
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            // Move this up
+            EditorGUI.BeginProperty(position, GUIContent.none, property);
+            {
+                property.isExpanded = EditorGUI.Foldout(new Rect(position.x, position.y, position.width, lineHeight),
+                    property.isExpanded, label);
+
+                if (property.isExpanded)
+                {
+                    position.height -= lineHeight;
+                    position.y += lineHeight;
+
+                    var sceneAssetProperty = GetSceneAssetProperty(property);
+
+                    position.height -= FOOTER_HEIGHT;
+                    GUI.Box(EditorGUI.IndentedRect(position), GUIContent.none, EditorStyles.helpBox);
+                    position = boxPadding.Remove(position);
+                    position.height = lineHeight;
+
+                    label.tooltip =
+                        "The actual Object reference.\nOn serialize this is also stored as the asset's path.";
+
+                    EditorGUI.BeginChangeCheck();
+                    {
+                        // removed the label here since we already have it in the foldout before
+                        sceneAssetProperty.objectReferenceValue = EditorGUI.ObjectField(position,
+                            sceneAssetProperty.objectReferenceValue, typeof(Object), false);
+                    }
+                    var objectReferenceValue = sceneAssetProperty.objectReferenceValue;
+                    if (EditorGUI.EndChangeCheck())
+                    {
+                        // If no valid scene asset was selected, reset the stored path accordingly
+                        if (objectReferenceValue == null) GetObjectPathProperty(property).stringValue = string.Empty;
+                    }
+
+                    position.y += paddedLine;
+
+                    /*if (!buildScene.assetGUID.Empty())
+                    {
+                        // Draw the Build Settings Info of the selected Scene
+                        DrawSceneInfoGUI(position, buildScene, sceneControlID + 1);
+                    }*/
+
+                }
+            }
+            EditorGUI.EndProperty();
+        }
+
+        /// <summary>
+        /// Ensure that what we draw in OnGUI always has the room it needs
+        /// </summary>
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            var sceneAssetProperty = GetSceneAssetProperty(property);
+            // Add an additional line and check if property.isExpanded
+            var lines = 2; // = property.isExpanded ? sceneAssetProperty.objectReferenceValue != null ? 3 : 2 : 1;
+
+            return boxPadding.vertical + lineHeight * lines + PAD_SIZE * (lines - 1) + FOOTER_HEIGHT;
+        }
+
+        /// <summary>
+        /// Draws info box of the provided scene
+        /// </summary>
+        private static SerializedProperty GetSceneAssetProperty(SerializedProperty property)
+        {
+            return property.FindPropertyRelative(sceneAssetPropertyString);
+        }
+
+        private static SerializedProperty GetObjectPathProperty(SerializedProperty property)
+        {
+            return property.FindPropertyRelative(scenePathPropertyString);
+        }
+    }
+}
+#endif
